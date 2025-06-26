2fe39004fc39acf3a51cdb72b74b8c6f1bd35aa8
Zachary Gaines <zacharygaines28@gmail.com>
2025-06-26 11:43:56 -0600

    still broken but if statement progress

--- main.py
+++ main.py
@@ -5,12 +5,50 @@
 # print(greet("World"))
 
 
-def eat(snack):
-        return f"I am eating {snack}."
+# def eat(snack):
+#         return f"I am eating {snack}."
 
 
-name = "Sandy"
-snack = "snickers"
-print("Hi, my name is " + name)
+# name = "Sandy"
+# snack = "snickers"
+# print("Hi, my name is " + name)
 
-print(name + " " + eat(snack))
+# print(name + " " + eat(snack))
+
+
+print("Welcome to my first game.");
+
+name = input("What is your name? ")
+age = int(input("What is your age? "))
+
+print(f"Hello {name} you are {age} years old!")
+
+
+is_older = age >= 18
+
+
+if age >= 18:
+  print("You are old enough to play!")
+elif age:
+  print("you are too young!")
+
+wants_to_play = input("Do you want to play? ")
+if wants_to_play == "yes":
+  print("Let's play!")
+# elif wants_to_play == "no":
+#     print("Go away")
+else:
+      print("I'm sorry", name,"you are not old enough to play!")
+
+
+
+
+
+
+'''
+Data types
+# str "hello", "what's up"
+# int 1, 2, 3
+# float 1.2, 76.87654
+# bool true or false
+'''
